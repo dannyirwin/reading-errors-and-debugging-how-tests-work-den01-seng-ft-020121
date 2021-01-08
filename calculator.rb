5e9d918bd93c4edaa18e465732583571c72cb342
Daniel Irwin <dirwin123@gmail.com>
2021-01-08 19:57:51 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
 # Add your variables here
 
 first_number = 5
-second_number = 1
+second_number = 99
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
